9b502c7102fb7cf33bc9ff3d89be4d2a4943a9a4
Sam Bowering <samjbowering@gmail.com>
2019-09-26 11:45:37 +1200

    Cleared unwanted existing code from boilerplate

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
 
-import ErrorMessage from './ErrorMessage'
-import LoadSubreddit from './LoadSubreddit'
-import SubredditList from './SubredditList'
-import WaitIndicator from './WaitIndicator'
-
 const App = () => (
   <div className='app'>
-    <ErrorMessage />
-    <LoadSubreddit>
-      <WaitIndicator />
-    </LoadSubreddit>
-    <SubredditList />
+    
   </div>
 )
 
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,5 @@
-import {combineReducers} from 'redux'
-
-import errorMessage from './error-message'
-import subreddits from './subreddits'
-import waiting from './waiting'
+import { combineReducers } from 'redux'
 
 export default combineReducers({
-  errorMessage,
-  subreddits,
-  waiting
+
 })
