d6f12b64a70e4005d6b05dbc17fa86ed46ad97e1
Jeremy Magill <jeremy.magill93@gmail.com>
2019-09-28 11:36:11 +1200

    Syntax fixes for SignUp.jsx component.

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import { GridForm, ColOne, ColTwo, Button } from './Styled'
 import { connect } from 'react-redux'
-import {register, isAuthenticated} from 'authenticare/client'
+import { register, isAuthenticated } from 'authenticare/client'
 
 class SignUp extends Component {
-
   state = {
     setForm: {
       username: '',
@@ -14,7 +13,8 @@ class SignUp extends Component {
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value })
-}
+  }
+
   handleClick = () => {
     register({
       username: this.state.setForm.username,
@@ -24,36 +24,36 @@ class SignUp extends Component {
     })
       .then((token) => {
         if (isAuthenticated()) {
-          props.history.push('/')
+          this.props.history.push('/')
         }
       })
   }
-  
-  render(props) {
+
+  render (props) {
     return (
       <div>
         <h2>Sign Up</h2>
-          <GridForm>
-            <ColOne>Username:</ColOne>
-            <ColTwo name='username'
-              // value={this.state.setForm.username}
-              placeholder='Username'
-              onChange={this.handleChange} />
+        <GridForm>
+          <ColOne>Username:</ColOne>
+          <ColTwo name='username'
+            // value={this.state.setForm.username}
+            placeholder='Username'
+            onChange={this.handleChange} />
 
-            <ColOne>Password:</ColOne>
-            <ColTwo name='password' type='password'
-              // value={this.state.setForm.password}
-              placeholder='Password'
-              onChange={this.handleChange} />
+          <ColOne>Password:</ColOne>
+          <ColTwo name='password' type='password'
+            // value={this.state.setForm.password}
+            placeholder='Password'
+            onChange={this.handleChange} />
 
-            <Button type='button' onClick={this.handleClick}>Sign in</Button>
-          </GridForm>
+          <Button type='button' onClick={this.handleClick}>Sign in</Button>
+        </GridForm>
       </div>
     )
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     form: {
       username: 'JAMES',
@@ -61,6 +61,5 @@ function mapStateToProps(state) {
     }
   }
 }
-
 
 export default connect(mapStateToProps)(SignUp)
