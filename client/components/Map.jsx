4ebda524856454624479e7fc05a1a6bb091306c8
James <james_wylie@outlook.com>
2019-09-27 19:02:16 +1200

    attempt multiple markers

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -20,14 +20,17 @@ const wrapperStyles = {
   width: "100%",
   maxWidth: "100%",
   margin: "0 auto",
+  fontFamily: "Roboto, sans-serif"
 }
+
+const { show, hide } = actions
 
 class Map extends Component {
   state = {
     center: [0,20],
     zoom: 1,
     countries: [
-      { name: "Andorra", coordinates: [1.601554,42.546245],  zoomTimes: 7 },
+      { name: "Andorra", coordinates: [1.601554,42.546245],  zoomTimes: 40 },
       { name: "United Arab Emirates", coordinates: [53.847818,23.424076],  zoomTimes: 4 },
       { name: "Afghanistan", coordinates: [67.709953,33.93911	] , zoomTimes: 2 },
       { name: "Antigua and Barbuda", coordinates: [-61.796428, 17.060816],  zoomTimes: 12 },
@@ -38,9 +41,10 @@ class Map extends Component {
       { name: "Angola", coordinates: [	17.873887	,	-11.202692	],  zoomTimes: 2 },
       { name: "Antarctica", coordinates: [-0.071389,-75.250973	],  zoomTimes: 2 },
       { name: "Argentina", coordinates: [-63.616672, -38.416097],  zoomTimes: 3 },
-      { name: "American Samoa", coordinates: [-170.132217	, -14.270972],  zoomTimes: 10},
+      { name: "American Samoa", coordinates: [-170.132217	, -14.270972],  zoomTimes: 40},
       { name: "Austria", coordinates: [14.550072,	47.516231],  zoomTimes: 1.5 },
       { name: "Australia", coordinates: [133.775136, -25.274398], zoomTimes: 3 },
+      { name: "Niue", coordinates: [	-169.867233, 	-19.054445	], zoomTimes: 40 },
       //https://developers.google.com/public-data/docs/canonical/countries_csv grabbing coordinates from here! lol long process.
     ]
   }
@@ -61,6 +65,34 @@ handleReset() {
     zoom: 1,
   })
 }
+
+handleZoomIn() {
+  this.setState({
+    zoom: zoom+1,
+  })
+}
+
+handleZoomOut() {
+  this.setState({
+    zoom: zoom-1,
+  })
+}
+
+// handleMove(geography, evt) {
+//   const x = evt.clientX
+//   const y = evt.clientY + window.pageYOffset
+//   this.props.dispatch(
+//     show({
+//       origin: { x, y },
+//       content: geography.properties.name,
+//     })
+//   )
+// }
+
+// handleLeave() {
+//   this.props.dispatch(hide())
+// }
+
   render() {
     return (
       <div className="div buttons-for-countries">
@@ -110,37 +142,48 @@ handleReset() {
           }}
         >
   <ZoomableGroup center={[x,y]} zoom={zoom}>
-    <Geographies geography={ "/countries-110m.json" }>
+    <Geographies geography={ "/countries-50m.json" }>
       {(geographies, projection) => geographies.map(geography => (
         <Geography 
         key={ geography.id } 
         geography={ geography } 
         projection={ projection } 
         onClick={() => console.log(geography.properties.name)}
+        onMouseMove={this.handleMove}
+        onMouseLeave={this.handleLeave}
         style={{
           default: {
             fill: "#ECEFF1",
-            stroke: "#607D8B",
-            strokeWidth: 0.5,
+            fillOpacity: 0,
+            stroke: "#000",
+            strokeWidth: 0.4,
             outline: "none",
           },
           hover: {
             fill: "#607D8B",
             stroke: "#607D8B",
-            strokeWidth: 0.5,
+            strokeWidth: 0.1,
             outline: "none",
           },
           pressed: {
             fill: "#FF5722",
             stroke: "#607D8B",
-            strokeWidth: 2.2,
+            strokeWidth: 0.1,
             outline: "none",
           }}} 
           />
       ))}
     </Geographies>
     <Markers>
-                {
+                { <Marker marker={{ coordinates: [ 8.5, 47.3 ] }}>
+                    <button onClick={() => this.handleZoomIn()}>ZOOM IN</button>
+                    
+                </Marker>}
+
+                < Marker marker={{ coordinates: [ 8.5, 47.3 ] }}>
+                <button onClick={() => this.handleZoomOut()>ZOOM OUT </button>
+// ISSUES HERE!!!
+                </Marker>
                   this.state.countries.map((country, i) => (
                     <Marker key={i} marker={country}>
                       <circle
@@ -158,11 +201,13 @@ handleReset() {
   </ZoomableGroup>
 </ComposableMap>
 )}
+
       </Motion>
+      {/* <Tooltip/> */}
       </div>
       </div>
     )
   }
 }
  
-export default Map
+export default Map 
