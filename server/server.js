0038ed9c604f52d7b5688e52e77b735b7a0ce4e4
Jeremy Magill <jeremy.magill93@gmail.com>
2019-09-28 11:28:43 +1200

    merge conflict

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const express = require('express')
 
+
+
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
+
+
 
 module.exports = server
