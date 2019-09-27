3bc250750fb44ac415f2041a35444db8c5c331ab
Sam Bowering <samjbowering@gmail.com>
2019-09-27 20:16:21 +1200

    Random background image on component mount

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Button } from 'semantic-ui-react'
 
-import SignIn from './SignIn'
+import { fetchBackgroundImage } from '../api/fetchBackgroundImage'
 
 class Home extends React.Component {
   state = {
     style: {
       backgroundImage: '',
+      backgroundSize: '100%, 100%',
+      backgroundPosition: 'center',
       height: '100vh',
       width: '100vw'
     }
   }
 
+  randomPhoto = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+
   componentDidMount () {
-    this.setState({
-      style: {
-        // backgroundImage:  this.props.getBackgroundImage()
-        backgroundImage: 'linear-gradient(red, yellow)',
-        height: '100vh',
-        width: '100vw'
-      }
-    })
+    const random = this.randomPhoto(1, 10)
+    fetchBackgroundImage()
+      .then(res => {
+        this.setState({style: {
+          backgroundSize: '100%, 100%',
+          backgroundPosition: 'center',
+          height: '100vh',
+          width: '100vw',
+          backgroundImage: `url(${res.body.results[random].urls.full})`
+        }})
+      })
+  }
+
+  handleClick = () => {
+
   }
 
   render () {
@@ -30,7 +45,7 @@ class Home extends React.Component {
       <div style={this.state.style}>
         <h1>Welcome</h1>
         <h2>Start creating your travel bucket list today.</h2>
-        <Link to='/signIn'><button className='ui button'>SIGN IN</button></Link>
+        <Link to='/signIn'><button className='ui button' onClick={this.handleClick}>SIGN IN</button></Link>
       </div>
     )
   }
@@ -38,7 +53,7 @@ class Home extends React.Component {
 
 function mapStateToProps (state) {
   return {
-    backgroundImage: state.backgroundImage,
+    // backgroundImage: state.backgroundImage,
     isAuthenticated: state.isAuthenticated
   }
 }
