27016cc3f76bfe9aea196fc3101002f4f8373923
nikolaii777 <rayynenz@gmail.com>
2019-09-27 15:04:50 +1200

    my changes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react"
+import ReactDOM from 'react-dom'
+import Header from './Header'
 
-const App = () => (
-  <div className='app'>
-    
-  </div>
-)
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <button onclick="myFunction()">Sign in</button>
+
+    </div>
+  )
+}
 
 export default App
+
---
+++ client/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+  return (
+    <Header>
+      <h3>This is my Header element</h3>
+      <button onclick="myFunction()">Sign in</button>
+    </Header>
+  )
+}
+
+export default Header
+
