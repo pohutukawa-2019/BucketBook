1c1ba24b798b6ab784458112b583f6dd48a4c064
Angela Yang <angela.yang02@gmail.com>
2019-09-27 15:20:44 +1200

    fixed header component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,17 +2,11 @@ import React from "react"
 import ReactDOM from 'react-dom'
 import Header from './Header'
 
-
 const App = () => (
-
-
   <div>
     <Header />
     <button onclick="myFunction()">Sign in</button>
-
   </div>
 )
 
-
 export default App
-
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,10 @@
 import React from "react"
 
-function Header() {
-  return (
-    <Header>
-      <h3>This is my Header element</h3>
-      <button onclick="myFunction()">Sign in</button>
-    </Header>
-  )
-}
+const Header = () => (
+  <div>
+    <h3>This is my Header element</h3>
+    <button onclick="myFunction()">Sign in</button>
+  </div>
+)
 
 export default Header
-
