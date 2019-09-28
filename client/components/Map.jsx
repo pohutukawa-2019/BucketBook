5c2d21679b0d9b10f479552c72cb38c56e37ccba
James <james_wylie@outlook.com>
2019-09-28 16:37:06 +1200

    Map Before Styling

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import SearchBar from './SearchBar'
-import Search from 'react-search'
+
 import {
   ComposableMap,
   ZoomableGroup,
@@ -12,19 +11,12 @@ import {
 
 import { Motion, spring } from "react-motion"
 
-import {
-  Tooltip,
-  actions,
-} from "redux-tooltip"
-
 const wrapperStyles = {
   width: "100%",
   maxWidth: "100%",
   margin: "0 auto",
   fontFamily: "Roboto, sans-serif"
 }
-
-const { show, hide } = actions
 
 class Map extends Component {
   state = {
@@ -36,18 +28,6 @@ class Map extends Component {
       { name: "Andorra", coordinates: [1.601554,42.546245],  zoomTimes: 40 },
       { name: "United Arab Emirates", coordinates: [53.847818,23.424076],  zoomTimes: 4 },
       { name: "Afghanistan", coordinates: [67.709953,33.93911	] , zoomTimes: 2 },
-      { name: "Antigua and Barbuda", coordinates: [-61.796428, 17.060816],  zoomTimes: 12 },
-      { name: "Anguilla", coordinates: [-63.068615,18.220554],  zoomTimes: 12 },
-      { name: "Albania", coordinates: [20.168331,	41.153332	] ,  zoomTimes: 2},
-      { name: "Armenia", coordinates: [45.038189	,40.069099	],  zoomTimes: 2},
-      { name: "Netherlands Antilles", coordinates: [-69.060087,12.226079], zoomTimes: 2},
-      { name: "Angola", coordinates: [	17.873887	,	-11.202692	],  zoomTimes: 2 },
-      { name: "Antarctica", coordinates: [-0.071389,-75.250973	],  zoomTimes: 2 },
-      { name: "Argentina", coordinates: [-63.616672, -38.416097],  zoomTimes: 3 },
-      { name: "American Samoa", coordinates: [-170.132217	, -14.270972],  zoomTimes: 40},
-      { name: "Austria", coordinates: [14.550072,	47.516231],  zoomTimes: 1.5 },
-      { name: "Australia", coordinates: [133.775136, -25.274398], zoomTimes: 3 },
-      { name: "Niue", coordinates: [	-169.867233, 	-19.054445	], zoomTimes: 40 },
       //https://developers.google.com/public-data/docs/canonical/countries_csv grabbing coordinates from here! lol long process.
     ]
   }
@@ -90,35 +70,15 @@ getCountryName(countryName) {
     activeCountry: countryName,
   })}
 
-
-  
-// handleMove(geography, evt) {
-//   const x = evt.clientX
-//   const y = evt.clientY + window.pageYOffset
-//   this.props.dispatch(
-//     show({
-//       origin: { x, y },
-//       content: geography.properties.name,
-//     })
-//   )
-// }
-
-// handleLeave() {
-//   this.props.dispatch(hide())
-// }
-
   render() {
     return (
       <div className="div buttons-for-countries">
-        <SearchBar countries={this.state.listOfCountries}/> 
       <div style={wrapperStyles}>
-    
         {
           this.state.countries.map((country, i) => (
             <button
               key={i}
               className="btn px1"
-              // edit styling further here
               data-country={i}
               onClick={this.handleCountrySelection}
               >
@@ -129,8 +89,7 @@ getCountryName(countryName) {
         <button onClick={this.handleReset}>
           { "Reset" }
         </button>
-        <button onClick={() => this.handleZoomIn()} onClick={() => this.handleZoomIn()}>ZOOM IN</button>  
-      
+        <button onClick={() => this.handleZoomIn()} onClick={() => this.handleZoomIn()}>ZOOM IN</button>     
         <button onClick={() => this.handleZoomOut()}>ZOOM OUT</button>
         <h1>{this.state.activeCountry}</h1>
       </div>
@@ -165,11 +124,10 @@ getCountryName(countryName) {
 
       {(geographies, projection) => geographies.map(geography => (
         <Geography 
-        key={ geography.id } 
+        key={ `heyMAMA${geography.properties.name}` } 
         geography={ geography } 
         projection={ projection } 
         countries={this.state.listOfCountries.push(geography.properties.name)}
-        onClick={() => console.log(this.state.listOfCountries)}
         onMouseEnter={() => this.getCountryName(geography.properties.name)}
         onMouseMove={this.handleMove}
         onMouseLeave={this.handleLeave}
@@ -214,9 +172,7 @@ getCountryName(countryName) {
   </ZoomableGroup>
 </ComposableMap>
 )}
-
       </Motion>
-
       </div>
       </div>
     )
--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Search from 'react-search'
 class Searchbar extends Component {
-   handleItemsChange(items) {
-     console.log(items)
+
+  state= {
+    repos: [{id: 0, value: 'Happfy'}, {id: 1, value: 'Sad'},{id: 3, value: 'Happrety'}, {id: 4, value: 'Sad'}, {id: 5, value: 'Happety'}, {id: 1, value: 'Sad'}, {id: 0, value: 'Hapgpy'}, {id: 1, value: 'Sad'}, {id: 0, value: 'Happerty'}, {id: 1, value: 'Sad'}, {id: 0, value: 'Happeyy'}, {id: 1, value: 'Sad'}, {id: 0, value: 'Hapgfpy'}, {id: 1, value: 'Sad'}],
+    selectedCountry: []
+
+    // {id: 0, value: 'Happy'}
+  }
+
+   handleItemsChange(country) {
+     console.log(country)
+    this.setState({selectedCountry: country})
    }
-   render (props) {
+
+   render() {
     
      return (
        <div>
            <h1>Searchbar</h1>
-         {/* <Search items={items} /> */}
-          <Search items={this.props.countries}
+          <Search items={this.state.repos}
                  placeholder='Pick your country'
                  maxSelected={1}
                  multiple={false}
