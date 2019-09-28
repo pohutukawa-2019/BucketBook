29ba93bbbeda5b52078e04f6c799eb4309c9c49b
Jeremy Magill <jeremy.magill93@gmail.com>
2019-09-28 14:27:27 +1200

    Fixed server.js linting errors.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const backgroundRoutes = require('./routes/background')
 
 const server = express()
 
-
 server.use(express.static(path.join(__dirname, '../public')))
 server.use('/api/v1/background', backgroundRoutes)
 
