b2d2be84b3d9b8517dee921554f0c79ffcd67bee
Sam Bowering <samjbowering@gmail.com>
2019-09-30 00:59:51 +1300

    Added social media assets

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Footer = () => {
   return(
     <div style={{backgroundColor: '#1B245C', height: '5vh', width: '100vw', position: 'absolute', bottom: '0px'}}>
-
+      <div style={{float: 'right', position: 'relative', padding: '1.5vh'}}>
+        <img src={'/images/BBook-footer-socmedia-nstagram.svg'} style={{right: '10vw', position: 'relative'}}></img>
+        <img src={'/images/BBook-footer-socmedia-twitter.svg'} style={{right: '8vw', position: 'relative'}}></img>
+        <img src={'/images/BBook-footer-socmedia-fb.svg'} style={{right: '6vw', position: 'relative'}}></img>
+      </div>
     </div>
   )
 }
