2889d8ea14d2f905f2421b8f62c25ffc38b548d0
Jeremy Magill <jeremy.magill93@gmail.com>
2019-09-29 16:34:16 +1300

    Cleaned up Map.jsx linting errors.

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 import {
   ComposableMap,
@@ -8,72 +8,69 @@ import {
   Geography,
   Markers,
   Marker
-} from "react-simple-maps"
+} from 'react-simple-maps'
 
-import { Motion, spring } from "react-motion"
-
-import { getCountry } from '../actions/country'
+import { Motion, spring } from 'react-motion'
 
 const wrapperStyles = {
-  width: "100%",
-  maxWidth: "100%",
-  margin: "0 auto",
-  fontFamily: "Roboto, sans-serif"
+  width: '100%',
+  maxWidth: '100%',
+  margin: '0 auto',
+  fontFamily: 'Roboto, sans-serif'
 }
 
 class Map extends Component {
   state = {
     listOfCountries: [],
-    center: [0,20],
+    center: [0, 20],
     zoom: 1,
     activeCountry: '',
     countries: [
-      { name: "Andorra", coordinates: [1.601554,42.546245],  zoomTimes: 40 },
-      { name: "United Arab Emirates", coordinates: [53.847818,23.424076],  zoomTimes: 4 },
-      { name: "Afghanistan", coordinates: [67.709953,33.93911	] , zoomTimes: 2 },
-      //https://developers.google.com/public-data/docs/canonical/countries_csv grabbing coordinates from here! lol long process.
+      { name: 'Andorra', coordinates: [1.601554, 42.546245], zoomTimes: 40 },
+      { name: 'United Arab Emirates', coordinates: [53.847818, 23.424076], zoomTimes: 4 },
+      { name: 'Afghanistan', coordinates: [67.709953, 33.93911], zoomTimes: 2 }
+      //  https://developers.google.com/public-data/docs/canonical/countries_csv grabbing coordinates from here! lol long process.
     ]
   }
   handleCountrySelection = this.handleCountrySelection.bind(this)
   handleReset = this.handleReset.bind(this)
 
-handleCountrySelection(evt) {
-  const countryId = evt.target.getAttribute("data-country")
-  const country = this.state.countries[countryId]
-  this.setState({
-    center: country.coordinates,
-    zoom: country.zoomTimes,
-  })
-}
-handleReset() {
-  this.setState({
-    center: [0,20],
-    zoom: 1,
-  })
-}
+  handleCountrySelection (evt) {
+    const countryId = evt.target.getAttribute('data-country')
+    const country = this.state.countries[countryId]
+    this.setState({
+      center: country.coordinates,
+      zoom: country.zoomTimes
+    })
+  }
+  handleReset () {
+    this.setState({
+      center: [0, 20],
+      zoom: 1
+    })
+  }
 
-handleZoomIn() {
-  this.setState({
-    // center: [0,20],
-    zoom: this.state.zoom+1,
-  })
-}
+  handleZoomIn () {
+    this.setState({
+      zoom: this.state.zoom + 1
+    })
+  }
 
-handleZoomOut() {
-  if(this.state.zoom !== 1){
-  this.setState({
-    // center: [0,20], 
-    zoom: this.state.zoom-1,
-  
-  })}
-}
+  handleZoomOut () {
+    if (this.state.zoom !== 1) {
+      this.setState({
+        zoom: this.state.zoom - 1
+      })
+    }
+  }
 
-getCountryName(countryName) {
-  this.setState({
-    activeCountry: countryName,
-  })}
+  getCountryName (countryName) {
+    this.setState({
+      activeCountry: countryName
+    })
+  }
 
-  render() {
+  render () {
     return (
       <div className="div buttons-for-countries">
         <div style={wrapperStyles}>
@@ -90,9 +87,9 @@ getCountryName(countryName) {
             ))
           }
           <button onClick={this.handleReset}>
-            { "Reset" }
+            { 'Reset' }
           </button>
-          <button onClick={() => this.handleZoomIn()} onClick={() => this.handleZoomIn()}>ZOOM IN</button>     
+          <button onClick={() => this.handleZoomIn()}>ZOOM IN</button>
           <button onClick={() => this.handleZoomOut()}>ZOOM OUT</button>
           <h1>{this.state.activeCountry}</h1>
         </div>
@@ -101,29 +98,29 @@ getCountryName(countryName) {
             defaultStyle={{
               zoom: 1,
               x: 0,
-              y: 20,
+              y: 20
             }}
             style={{
-              zoom: spring(this.state.zoom, {stiffness: 210, damping: 20}),
-              x: spring(this.state.center[0], {stiffness: 210, damping: 20}),
-              y: spring(this.state.center[1], {stiffness: 210, damping: 20}),
+              zoom: spring(this.state.zoom, { stiffness: 210, damping: 20 }),
+              x: spring(this.state.center[0], { stiffness: 210, damping: 20 }),
+              y: spring(this.state.center[1], { stiffness: 210, damping: 20 })
             }}
           >
-            {({zoom,x,y}) => (
+            {({ zoom, x, y }) => (
               <ComposableMap
                 projectionConfig={{
                   scale: 205,
-                  rotation: [-11,0,0],
+                  rotation: [-11, 0, 0]
                 }}
                 width={980}
                 height={551}
                 style={{
-                  width: "100%",
-                  height: "auto",
+                  width: '100%',
+                  height: 'auto'
                 }}
               >
-                <ZoomableGroup center={[x,y]} zoom={zoom}>
-                  <Geographies geography={ "/countries-50m.json" }>
+                <ZoomableGroup center={[x, y]} zoom={zoom}>
+                  <Geographies geography={ '/countries-50m.json' }>
 
                     {(geographies, projection) => geographies.map(geography => (
                       <Link key={geography.id} to={`/country/${geography.properties.name}`}>
@@ -137,23 +134,23 @@ getCountryName(countryName) {
                           onMouseLeave={this.handleLeave}
                           style={{
                             default: {
-                              fill: "#ECEFF1",
+                              fill: '#ECEFF1',
                               fillOpacity: 0,
-                              stroke: "#FFF",
+                              stroke: '#FFF',
                               strokeWidth: 0.8,
-                              outline: "none",
+                              outline: 'none'
                             },
                             hover: {
-                              fill: "#f7b731",
-                              stroke: "#607D8B",
+                              fill: '#f7b731',
+                              stroke: '#607D8B',
                               strokeWidth: 0.1,
-                              outline: "none",
+                              outline: 'none'
                             },
                             pressed: {
-                              fill: "#FF5722",
-                              stroke: "#607D8B",
+                              fill: '#FF5722',
+                              stroke: '#607D8B',
                               strokeWidth: 0.1,
-                              outline: "none",
+                              outline: 'none'
                             }
                           }}
                         />
