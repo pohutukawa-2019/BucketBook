d0831e418f12f9094758cfc697430ddf42411912
James <james_wylie@outlook.com>
2019-09-29 15:55:14 +1300

    api function for fetch bucketlist item

--- client/api/fetchbucketlistitems.js
+++ client/api/fetchbucketlistitems.js
@@ -1,9 +1,12 @@
 import makeRequest from './requestor'
+import {getEncodedToken} from 'authenticare/client'
 
-const bucketListItemsPath = '/bucketlist'
+const bucketListItemsPath = '/api/v1/bucketlist'
 
 export function fetchBucketListItemsByCountry (selectedCountry) {
   return makeRequest(`${bucketListItemsPath}/${selectedCountry}`)
+    .set({ 'Accept': 'application/json' })
+    .set({ 'Authorization': `Bearer ${getEncodedToken()}` })
     .then(res => res.body)
     .catch(() => { throw new Error('Error accessing bucket list items') })
 }
