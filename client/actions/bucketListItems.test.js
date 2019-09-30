e851fe74f099d45697ac52ad7d42846b22afc57e
Jeremy Magill <jeremy.magill93@gmail.com>
2019-10-01 10:38:44 +1300

    getBucketList thunk action tested and working as intended.

--- client/actions/bucketListItems.test.js
+++ client/actions/bucketListItems.test.js
@@ -1,9 +1,20 @@
 import {
   getBucketListPending,
   getBucketListSuccess,
+  getBucketList,
   GET_BUCKETLIST_PENDING,
   GET_BUCKETLIST_SUCCESS
 } from './bucketListItems'
+
+jest.mock('../api/requestor', () => {
+  return (endpoint, method, payload) => {
+    const selectedCountry = endpoint.split('/').pop()
+
+    if (selectedCountry === 'Zambia') {
+      return Promise.resolve([{ id: 1, title: 'Swim' }, { id: 2, title: 'Run' }])
+    }
+  }
+})
 
 describe('bucketListItem action tests', () => {
   it('getBucketListPending returns a GET_BUCKETLIST_PENDING action', () => {
@@ -16,5 +27,16 @@ describe('bucketListItem action tests', () => {
     const action = getBucketListSuccess(bucketListDetails)
     expect(action.type).toBe(GET_BUCKETLIST_SUCCESS)
     expect(action.bucketList).toBe(bucketListDetails)
+  })
+
+  it('getBucketlist dispatches the correct actions', () => {
+    const dispatch = jest.fn()
+    const selectedCountry = 'Zambia'
+
+    return getBucketList(selectedCountry)(dispatch)
+      .then(() => {
+        expect(dispatch.mock.calls[0][0].type).toBe(GET_BUCKETLIST_PENDING)
+        expect(dispatch.mock.calls[1][0].type).toBe(GET_BUCKETLIST_SUCCESS)
+      })
   })
 })
