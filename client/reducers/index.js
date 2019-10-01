08d22d14949f1b2f85f54afdb8df8ab6627e964b
Jeremy Magill <jeremy.magill93@gmail.com>
2019-10-01 17:37:24 +1300

    Waiting reducer created and getBackground action types added as switch cases.

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 import selectedCountry from './country'
 import bucketList from './bucketListItems'
 import background from './background'
+import waiting from './waiting'
 
 export default combineReducers({
   selectedCountry,
   bucketList,
-  background
+  background,
+  waiting
 })
